9293b16cfecebfcfa5dbe1e8606d2cb2ef53683b
城岡友基 <yukipetz@petz.local>
2020-06-02 19:31:13 +0900

    Add embed youtube playlist

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,16 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_user, only: :songs
+
   def home
   end
 
   def songs
-    # if logged_in_user
       require 'google/apis/youtube_v3'
       youtube = Google::Apis::YoutubeV3::YouTubeService.new
       youtube.key = "AIzaSyBZLo8NpskFXqqAAASUwOHuYpBf7XmP8TQ"
-      youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: "black music funk soul", max_results: 120)
+      youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: "black music funk soul", max_results: 30)
       search_result = youtube_search_list.to_h
       @movies = search_result[:items]
-    # end
   end
 
   def about
