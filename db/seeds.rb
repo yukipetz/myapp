e57eafce723c9c9c55e912435c0aab073ceff21a
城岡友基 <yukipetz@petz.local>
2020-07-22 21:57:33 +0900

    Edit admin user information

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+             email: "example@myapp.org",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
